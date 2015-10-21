d88a4ef8cda54e524a61e880afcd140333803a06
gikuyakat <goto.etal.exp@gmail.com>
2015-10-21 18:47:02 +0900

    151021_2

---
+++ R/mllavartest.R
@@ -0,0 +1,19 @@
+#' data example for MLLaVar
+#'
+#'
+#' The variables are follows:
+#'
+#' \itemize{
+#'   \item group group variable
+#'   \item time independent variable (between level)
+#'   \item collectivism independent variable 1
+#'   \item individualism independent variable 2
+#'   \item satisfaction dependent variable
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name mllavartest
+#' @usage data(mllavartest)
+#' @format A data frame with 163 rows and 5 variables
+NULL
